73bb9ac8e56b30ed137d754b988ad4f4b8447deb
William Wedler <wwedler@aquion-energy.com>
2014-07-03 16:52:51 -0400

    added helper scripts for dumpcpp solidworks libraries

---
+++ script/sldlib_id.bat
@@ -0,0 +1,7 @@
+:: Fix based on http://qt-project.org/forums/viewthread/13963
+C:\Qt\Qt5.2.1\5.2.1\msvc2012_64_opengl\bin\dumpcpp.exe -getfile {83A33D31-27C5-11CE-BFD4-00400513BB57}
+C:\Qt\Qt5.2.1\5.2.1\msvc2012_64_opengl\bin\dumpcpp.exe -getfile {4687F359-55D0-4CD3-B6CF-2EB42C11F989}
+C:\Qt\Qt5.2.1\5.2.1\msvc2012_64_opengl\bin\dumpcpp.exe -getfile {0d825e02-9000-4d82-b4ab-d6bdc2872797} 
+
+C:\Qt\Qt5.2.1\5.2.1\msvc2012_64_opengl\bin\dumpcpp.exe -h
+pause
